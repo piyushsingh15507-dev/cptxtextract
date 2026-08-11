a769219780884a68405f53a2b5c0f2a7857bf584
piyushsingh15507-dev <piyushsingh15507@gmail.com>
2026-08-12 03:33:09 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,10 +2,10 @@ import os
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", 25218674))
-API_HASH = getenv("API_HASH", "87c231f0e8704795b8239d06965b4351")
-BOT_TOKEN = getenv("BOT_TOKEN", "7198600769:AAGW4WAlCWppTjwTceBxWqDwpWP7UCEWuzI")
-OWNER_ID = int(getenv("OWNER_ID", "6805344720"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "999741495  20000898623").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002038285084"))
+API_ID = int(getenv("API_ID", 36317039))
+API_HASH = getenv("API_HASH", "63a3043f21587a8ef3d341d3901d86ab")
+BOT_TOKEN = getenv("BOT_TOKEN", "5887516185:AAHbYz8_89S0N2h2vzaQxQHxf1XjZ_o-10o")
+OWNER_ID = int(getenv("OWNER_ID", "6821417645"))
+SUDO_USERS = 6821417645
+MONGO_URL = getenv("MONGO_DB", "mongodb+srv://piyushsingh15507_db_user:tC7SZxBcBiXnVxU3@cpext.m6cymxf.mongodb.net/?retryWrites=true&w=majority&appName=CPEXT")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003553512208"))
